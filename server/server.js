90fcbc8fe3f9eb2d9b39d323b501bd49f9464d35
mneunomne <alberto.harres@gmail.com>
2021-01-24 03:01:27 +0100

    make local api

---
+++ server/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const fs = require('fs');
+
+const app = express()
+
+let db_data = fs.readFileSync('db/data.json')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/data', function (req, res) {
+  let obj = JSON.parse(db_data);
+  res.json(obj);
+})
+
+app.get('/speaker_id/:id', function (req, res) {
+  let id = parseInt(req.params.id)
+  let obj = JSON.parse(db_data);
+  let audios = obj.audios.filter(a => {
+    return a.from_id === id
+  })
+  res.json(audios);
+})
+
+app.get('/speaker/:name', function (req, res) {
+  let name = req.params.name
+  let obj = JSON.parse(db_data);
+  let audios = obj.audios.filter(a => a.from.toLowerCase().includes(name))
+  res.json(audios);
+})
+
+app.listen(3000, (err) => {
+  if (err) throw err
+  console.log('Server running in http://127.0.0.1:3000')
+})
+
+
+
+module.exports = app
