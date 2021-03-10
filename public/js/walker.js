b53f8a4145867dce591e9108ad4810aae3624ccd
mneunomne <alberto.harres@gmail.com>
2021-03-10 22:23:29 +0100

    update walker

--- public/js/walker.js
+++ public/js/walker.js
@@ -1,3 +1,33 @@
+
+var data; 
+
+const colors = [ 
+	"#FF0000",
+	"#FF7F00",
+	"#FFD400",
+	"#FFFF00",
+	"#BFFF00",
+	"#6AFF00",
+	"#00EAFF",
+	"#0095FF",
+	"#0040FF",
+	"#AA00FF",
+	"#FF00AA",
+	"#EDB9B9",
+	"#E7E9B9",
+	"#B9EDE0",
+	"#B9D7ED",
+	"#DCB9ED",
+	"#8F2323",
+	"#8F6A23",
+	"#4F8F23",
+	"#23628F",
+	"#6B238F",
+	"#000000",
+	"#737373",
+	"#CCCCCC"
+]
+
 class Walker {
   constructor(w, h) {
     this.simplex = new SimplexNoise()
@@ -20,18 +50,30 @@ class Walker {
   }
 }
 
-function createWalker() {
-  let div = document.createElement('div')
-  div.setAttribute("class", "ball")
-  let a = document.querySelector("#area").append(div)
+function createWalker(s, i) {
+  // circle
+  let circle = document.createElement('div')
+  circle.setAttribute("class", "ball")
+  document.querySelector("#area").append(circle)
+
+  let circle2 = document.createElement('div')
+  circle2.setAttribute("class", "ball")
+  document.querySelector("#names").append(circle2)
+  circle2.setAttribute("style", `background: ${colors[i]}`)
+  circle2.innerText = s.speaker
+
+  //document.querySelector("#names").append(circle)
+  
+  // walk
   let walker = new Walker(300, 300)
-  // this.walker.step()
   setInterval(() => {
     let pos = walker.step()
-    div.setAttribute("style", `top: ${pos.y}px; left: ${pos.x}px`)
+    circle.setAttribute("style", `top: ${pos.y}px; left: ${pos.x}px; background: ${colors[i]}`)
   }, 50)
-} 
-
-for(let i = 0; i < 24; i++) {
-  createWalker()
 }
+
+$.getJSON("db/data.json", function(json) {
+  data = json; // this will show the info it in firebug console
+  console.log('data', data)
+  data.speakers.map((s,i) => createWalker(s, i))
+});
