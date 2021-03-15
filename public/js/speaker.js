055a2e87b486b0efd268db98dd36f73fb21c09e2
mneunomne <alberto.harres@gmail.com>
2021-03-15 01:43:18 +0100

    angle words

--- public/js/speaker.js
+++ public/js/speaker.js
@@ -25,7 +25,6 @@ class NoiseWalker {
 
 class Orbiter {
   constructor (w, h, index) {
-    console.log('index ', index)
     this.time = 0
     this.centre = createVector(0,0)
     this.pos = createVector( 50 + (index  * 10),50)
@@ -37,7 +36,7 @@ class Orbiter {
   update() {
     var acc = this.centre.copy()
     acc.sub(this.pos)
-    acc.setMag(0.15)
+    acc.setMag(0.03)
     this.vel.add(acc)
     this.pos.add(this.vel)
     // console.log('this.pos',this.pos.x, this.pos.y)
@@ -53,9 +52,25 @@ class Orbiter {
 
 
 class Speaker extends Orbiter {
-  constructor(w, h, color, index) {
+  constructor(w, h, color, index, s_data) {
+    console.log('speaker data', w, h, color, index, s_data)
     super(w, h, index)
     this.color = color
+    this.id = s_data.id
+    this.addEvents()
+    this.curText = ''
+  }
+  addEvents () {
+    document.addEventListener(`play_audio_${this.id}`, this.onPlay.bind(this))
+    document.addEventListener(`audio_ended_${this.id}`, this.onEnd.bind(this))
+  }
+  onPlay (evt) {
+    this.curText = evt.detail.audio.text
+    console.log('onPlay',evt.detail.audio.text, this.curText)
+  }
+  onEnd (evt) {
+    console.log('onEnd', evt)
+    this.curText = ''
   }
   draw () {
     /*
@@ -67,11 +82,83 @@ class Speaker extends Orbiter {
     }
     */
     // fill(this.color)
-    stroke(this.color)
+    // stroke(this.color)
     if (this.points.length < 2) return
     let p1 = this.points[this.points.length-2]
     let p2 = this.points[this.points.length-1]
-    line(p1.x, p1.y, p2.x, p2.y)
+    push();
+      translate(p2.x, p2.y)    
+      let a = atan2(p2.y - height/2, p2.x - width/2);
+      // console.log('a ', a )
+      rotate(90)
+      rotate(a)
+      // line(p1.x, p1.y, p2.x, p2.y)
+      let tw = textWidth(this.curText)
+      translate(-tw/2, -8)
+      text(this.curText, 0, 0)
+      // rect(0, 0, 10, 10);
+    pop();
     // ellipse(this.x, this.y, 5, 5);
+  }
+}
+
+class Orchestration {
+  constructor(audios) {
+    // all audios
+    this.audios = audios
+    // speakers with all audios
+    this.speakers = {}
+    audios.map((a) => {
+      if (this.speakers[a.from_id] == undefined) this.speakers[a.from_id] = []
+      this.speakers[a.from_id].push(a)
+    })
+    // interval counter
+    this.counter = 0
+  }
+  playAudiosWithInterval (num_audios, interval) {
+    var play_next = () => {
+      this.counter++
+      if (this.counter > num_audios) {
+        this.counter = 0
+        return
+      } 
+      this.play(this.getRandomAudio(), interval, play_next)
+    }
+    this.play(this.getRandomAudio(), interval, play_next)
+  }
+
+  getRandomAudio () {
+    return this.audios[Math.floor(Math.random() * this.audios.length)]
+  }
+
+  play (audio, interval, callback) {
+    let play_id = Math.floor(Math.random() + 10000)
+    this.send(audio, play_id)
+    setTimeout(() => {
+      this.onEnd(audio, play_id)
+      setTimeout(() => {
+        callback()
+      }, interval * 1000)
+    }, (audio.duration_seconds + 1) * 1000)
+  }
+
+  send (audio, play_id) {
+    var event = new CustomEvent(`play_audio_${audio.from_id}`, {
+      detail: {
+        audio: audio,
+        play_id: play_id
+      }
+    })
+    document.dispatchEvent(event)
+  }
+
+  onEnd (audio, play_id) {
+    var event = new CustomEvent(`audio_ended_${audio.from_id}`, {
+      detail: {
+        audio: audio,
+        play_id: play_id
+      }
+    })
+    document.dispatchEvent(event)
   }
 }
--- public/sketches/sketch.js
+++ public/sketches/sketch.js
@@ -1,23 +1,38 @@
 var data; 
-const colors = ["#e3a882","#FF7F00","#FFD400","#25e44d","#BFFF00","#6AFF00","#00EAFF","#0095FF","#0040FF","#AA00FF","#FF00AA","#EDB9B9","#E7E9B9","#B9EDE0","#B9D7ED","#DCB9ED","#8F2323","#8F6A23","#4F8F23","#23628F","#6B238F","#d1751f","#737373","#CCCCCC"]
+const colors = ["#e3a882","#FF7F00","#FFD400","#25e44d","#BFFF00","#6AFF00","#00EAFF","#0095FF","#0040FF","#AA00FF","#FF00AA","#EDB9B9","#E7E9B9","#B9EDE0","#B9D7ED","#DCB9ED","#8F2323","#8F6A23","#4F8F23","#23628F","#6B238F","#d1751f","#737373","#CCCCCC", "#2F8F23"]
 
 var _width = window.innerWidth
 var _height = window.innerHeight
 
 var speakers = []
 
+var orchestration
+
+
+$.getJSON("db/data.json", function(json) {
+  data = json; // this will show the info it in firebug console
+});
+
 function setup() {
-  createCanvas(_width, _height);
-  for (let i = 0; i < 24; i++) {
-    createSpeaker(i)
-  }
+  createCanvas(_width, _height, P2D);
+
+  data.speakers.map((s_data,i) => createSpeaker(s_data, i))
+  
+  orchestration = new Orchestration(data.audios)
+  orchestration.playAudiosWithInterval(50, 1)
+  orchestration.playAudiosWithInterval(50, 3)
+  orchestration.playAudiosWithInterval(50, 5)
   background(0, 0, 0)
+  textFont('Arial');
+  textSize(18)
+  fill(255)
+  angleMode(DEGREES)
+
 }
 
-function createSpeaker(i) {
-  console.log('i',i)
+function createSpeaker(s_data, i) {
   // walk
-  let speaker = new Speaker(_width, _height, colors[i], i)
+  let speaker = new Speaker(_width, _height, colors[i], i, s_data)
   speakers.push(speaker)
 }
 
@@ -26,11 +41,11 @@ function update() {
     speakers[i].update()
   }
 }
-
+var _i = 0
 function draw() {
-  // background(0, 0, 0)
+  background(0)
   update()
   for (let i = 0; i < speakers.length; i++) {
     speakers[i].draw()
-  }
+  }  
 }
