3b6f97920c5ad7e5c5f913697dd677a3c14c68dd
mneu windows <alberto.harres@gmail.com>
2021-08-23 15:38:13 +0200

    adjusts

--- form/script.js
+++ form/script.js
@@ -1,5 +1,6 @@
-var wavesurfer = WaveSurfer.create({
-    container: '#waveform',
+
+var wavesurferMic = WaveSurfer.create({
+    container: '#waveformMic',
     waveColor: '#000000',
     progressColor: '#46B54D',
     barHeight: 2,
@@ -9,12 +10,53 @@ var wavesurfer = WaveSurfer.create({
       ]
 });
 
-// wavesurfer.microphone.start();
-
-wavesurfer.microphone.on('deviceReady', function(stream) {
-    console.log('Device ready!', stream);
+var wavesurferAudio = WaveSurfer.create({
+    container: '#waveformAudio',
+    waveColor: '#000000',
+    progressColor: '#46B54D',
+    barHeight: 2,
+    barWidth: 1,
 });
-wavesurfer.microphone.on('deviceError', function(code) {
+
+wavesurferMic.microphone.start();
+
+var mediaRecorder = null;
+
+var stream = false
+
+$("#record").on('click', function () {
+    if (!stream) return
+
+    $("#waveformAudio").hide()
+    $("#waveformMic").show()
+
+    var audioChunks = []
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.start();
+    mediaRecorder.addEventListener('dataavailable', event=>{
+        audioChunks.push(event.data);              
+    })
+
+    mediaRecorder.addEventListener("stop", () => {
+        const audioBlob = new Blob(audioChunks);
+        const audioUrl = URL.createObjectURL(audioBlob);
+        const audio = new Audio(audioUrl);
+        // audio.play();
+        wavesurferAudio.load(audioUrl)
+        $("#waveformMic").hide()
+        $("#waveformAudio").show()
+    });
+})
+
+$("#stop").on('click', function () {
+    mediaRecorder.stop()
+})
+
+wavesurferMic.microphone.on('deviceReady', function(s) {
+    console.log('Device ready!', stream);  
+    stream = s
+});
+wavesurferMic.microphone.on('deviceError', function(code) {
     console.warn('Device error: ' + code);
 });
 
