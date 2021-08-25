3862b3e8b9942a86a3ce8826be827f8eb0cd1a91
mneu windows <alberto.harres@gmail.com>
2021-08-25 18:39:56 +0200

    working contribution form with update on database

--- form/script.js
+++ form/script.js
@@ -1,10 +1,13 @@
 
 var wavesurferMic = WaveSurfer.create({
     container: '#waveformMic',
-    waveColor: '#000000',
-    progressColor: '#46B54D',
+    waveColor: '#ffffff',
+    progressColor: '#000000',
     barHeight: 2,
-    barWidth: 5,
+    barWidth: 2,
+    backgroundColor: 'black',
+    cursorWidth: 0,
+    interact: false,
     plugins: [
         WaveSurfer.microphone.create()
       ]
@@ -12,20 +15,79 @@ var wavesurferMic = WaveSurfer.create({
 
 var wavesurferAudio = WaveSurfer.create({
     container: '#waveformAudio',
-    waveColor: '#000000',
+    waveColor: '#ffffff',
     progressColor: '#46B54D',
     barHeight: 2,
-    barWidth: 1,
+    barWidth: 2,
+    mediaControls: true
 });
 
-wavesurferMic.microphone.start();
+var $record = $("#record")
+var $stop = $("#stop")
+var $again = $("#again")
+var $start = $("#start")
+var $play = $("#play")
+var $submit = $("#submit")
+var $textInput = $("#textInput")
+var $languageInput = $("#languageInput")
+var $otherLanguage = $("#otherLanguage")
 
 var mediaRecorder = null;
-
 var stream = false
+var audioBlob = null
 
-$("#record").on('click', function () {
+const maxRecordingTime = 10000
+var startRecordingTimestamp = null
+
+
+const init = function () {
+    addEvents()
+    start()
+}
+
+const addEvents = function () {
+    $record.on('click', startRecording)
+    $stop.on('click', onStop)
+    $again.on('click', function () {
+        audioBlob = null
+        $again.hide()
+        $submit.addClass("disabled")
+        startRecording()
+    })
+
+    var removeInvalid = function (el) {
+        $( this ).removeClass("is-invalid")
+    }
+    $textInput.change(removeInvalid)
+    $languageInput.change(removeInvalid)
+    $otherLanguage.change(removeInvalid)
+
+    $play.on('click', onPlay)
+    $submit.on('click', onSubmit)
+    wavesurferMic.microphone.on('deviceReady', function(s) {
+        console.log('Device ready!', stream);  
+        stream = s
+    });
+    wavesurferMic.microphone.on('deviceError', function(code) {
+        console.warn('Device error: ' + code);
+    });
+}
+
+const start = function () {
+    wavesurferMic.microphone.start();
+    $record.show()
+}
+
+const startRecording = function () {
     if (!stream) return
+
+    startRecordingTimestamp = Date.now()
+
+    wavesurferMic.setWaveColor('#46B54D')
+
+    $record.hide()
+    $play.hide()
+    $stop.show()
 
     $("#waveformAudio").hide()
     $("#waveformMic").show()
@@ -37,8 +99,18 @@ $("#record").on('click', function () {
         audioChunks.push(event.data);              
     })
 
+    window.timer = setInterval(function () {
+        console.log(Date.now() - startRecordingTimestamp)
+        if (Date.now() - startRecordingTimestamp > maxRecordingTime) {
+            onStop();
+            clearInterval(window.timer)
+            window.timer = null
+        }
+    }, 100)
+
     mediaRecorder.addEventListener("stop", () => {
-        const audioBlob = new Blob(audioChunks);
+        audioBlob = new Blob(audioChunks, {type: 'audio/wav'});
+        console.log("audioBlob", audioBlob)
         const audioUrl = URL.createObjectURL(audioBlob);
         const audio = new Audio(audioUrl);
         // audio.play();
@@ -46,17 +118,117 @@ $("#record").on('click', function () {
         $("#waveformMic").hide()
         $("#waveformAudio").show()
     });
-})
+} 
 
-$("#stop").on('click', function () {
+const onStop = function () {
+    if (window.timer) clearInterval(window.timer)
+    wavesurferMic.setWaveColor('white')
+    $stop.hide()
+    $again.show()
+    $play.show()
+    $submit.removeClass("disabled")
     mediaRecorder.stop()
-})
+    
+}
 
-wavesurferMic.microphone.on('deviceReady', function(s) {
-    console.log('Device ready!', stream);  
-    stream = s
-});
-wavesurferMic.microphone.on('deviceError', function(code) {
-    console.warn('Device error: ' + code);
-});
 
+const onPlay = function () {
+    wavesurferAudio.playPause();
+}
+
+const server_url = `http://localhost:3000`
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+
+const getUserId = function () {
+    var voice_id = localStorage.getItem('voice_id')
+    if (voice_id === null) {
+        // new id
+        voice_id = makeid(8);
+        localStorage.setItem('voice_id', voice_id)
+    }
+    return voice_id;
+}
+
+
+var textInput, languageInput, otherLanguage
+
+const validateFields = function () {
+    var isValid = true
+    
+    textInput = $textInput.val()
+    languageInput = $languageInput.val()
+    otherLanguage = $otherLanguage.val()
+
+    console.log('validateFields', textInput, languageInput, otherLanguage)
+
+    if (audioBlob === null) {
+        isValid = false
+    }
+
+    if (textInput.length === 0) {
+        $textInput.addClass('is-invalid')
+        isValid = false
+    }
+    if (languageInput === null && otherLanguage.length === 0) {
+        $languageInput.addClass('is-invalid')
+        isValid = false
+    }
+    return isValid
+}
+
+const onSubmit = function () {
+    
+    var isValid = validateFields()
+
+    if (isValid === false) return
+
+    $submit.addClass("disabled")
+
+    let id = getUserId();
+    console.log("audioBlob", audioBlob.text())
+
+    var reader = new FileReader();
+    reader.onload = function(event){
+
+        var fd = new FormData();
+        fd.append('data', event.target.result);
+        fd.append('id', id);
+        fd.append('textInput', textInput);
+        fd.append('languageInput', languageInput);
+        fd.append('otherLanguage', otherLanguage);
+
+        $.ajax({
+            type: "POST",
+            url: `${server_url}/api/audio`,
+            data: fd,
+            processData: false,
+            contentType:false,
+            success: onPostSuccess,
+            error: onPostError,
+          });
+    }
+
+    reader.readAsDataURL(audioBlob);
+
+}
+
+const onPostSuccess = function (res) {
+    console.log("Sucess!", res)
+}
+const onPostError = function (res) {
+    console.error("Error!", res)
+}
+
+
+init()
--- server/post.js
+++ server/post.js
@@ -1,20 +1,149 @@
 const express = require('express')
+var cors = require('cors');
 const fs = require('fs');
+const multer = require('multer');
+const { exec } = require("child_process");
+const { getAudioDurationInSeconds } = require('get-audio-duration');
 
+
+const upload = multer();
 const app = express()
-
 const port = process.env.PORT || 3000
+const dest_folder = 'public/db_test'
 
-let db_data = fs.readFileSync('public/db/data.json')
+let json_data = fs.readFileSync('public/db_test/data.json')
+let db_data = JSON.parse(json_data)
 
+app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(express.static('form'))
 
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
 // POST
-app.post('/api/audio', function (req, res) {
-    res.status(200)
+app.post('/api/audio', upload.none(), function (req, res) {
+    console.log("request body", req.body)
+    var userId = req.body.id
+    var audioBlob = req.body.data
+    var lang_code = req.body.languageInput === 'null' ? '' : req.body.languageInput
+    var lang_other = req.body.otherLanguage === 'null' ? '' : req.body.otherLanguage
+    var text = req.body.textInput
+
+    // Validation
+
+    if (audioBlob === undefined || audioBlob === null || !audioBlob.includes('data:audio/wav;base64')) {
+        // error in audio blob
+         res.end('{"error" : "error in audio blob", "status" : 400}');
+         return
+    }
+
+    if (userId === '' || userId === null || userId === undefined) {
+        // error in userId
+         res.end('{"error" : "error in userId", "status" : 400}');
+         return
+    }
+
+    if ((lang_code === null || lang_code === undefined || lang_code === '') && (lang_other === null || lang_other === undefined || lang_other === '') ) {
+        // error in lang
+         res.end('{"error" : "error in lang", "status" : 400}');
+         return
+    }
+
+    if (text === null || text === undefined || text === '') {
+        // error in text
+         res.end('{"error" : "error in text", "status" : 400}');
+         return
+    }
+    
+    var base64data = audioBlob.split(",")[1]
+    var timestamp = Date.now()
+    var userFolder = `${dest_folder}/audios/${userId}`
+    // create the user folder if needed
+    if (!fs.existsSync(userFolder)){
+        fs.mkdirSync(userFolder);
+    }
+    
+    var filepath_tmp=`${userFolder}/${timestamp}_tmp.wav`
+    var filepath = `${userFolder}/${timestamp}.wav`
+    fs.writeFile(filepath_tmp, base64data, {encoding: 'base64'}, function(err) {
+        console.log('File created');
+        // convert audio file
+        exec(`ffmpeg -y -i ${filepath_tmp} ${filepath} -ar 48000`, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`conversion error: ${error.message}`)
+                return
+            }
+            if (stderr) {
+                // delete tmp file
+                fs.unlinkSync(filepath_tmp)
+                console.log(`conversion stderr: ${stderr}`)
+
+                getAudioDurationInSeconds(filepath).then((duration) => {                    
+                    // language
+                    var audio_id = makeid(8)
+                    
+                    writeData({
+                        name: "",
+                        user_id: userId,
+                        filepath: filepath,
+                        id: audio_id,
+                        text: text,
+                        duration_seconds: duration,
+                        lang_code: lang_code,
+                        lang_other: lang_other,
+                    })
+
+                    // res.status(200)
+                    res.end('{"success" : "Submited new audio", "status" : 200}');
+                });
+
+            }
+        })
+    });
 })
+
+const writeData = function (data) {
+    console.log('data', data)
+    
+    var audioObj = {
+        "from": data.name,
+        "file": data.filepath,
+        "id": data.id,
+        "text": data.text,
+        "from_id": data.user_id,
+        "duration_seconds": data.duration_seconds,
+        "lang": {
+            "name": data.lang_other,
+            "code": data.lang_code,
+            "standard":""
+        }
+    }
+    
+    db_data.audios.push(audioObj)
+    
+    // check if user data is already in db
+    if (!db_data.speakers.some(s => s.id === data.user_id)) {
+        var userObj = {
+            "speaker": data.name,
+            "id": data.user_id
+        }
+        db_data.speakers.push(userObj)
+    }
+
+    let json_data = JSON.stringify(db_data, null, 4);
+    fs.writeFileSync(`${dest_folder}/data.json`, json_data)
+}
 
 app.listen(port, (err) => {
     if (err) throw err
