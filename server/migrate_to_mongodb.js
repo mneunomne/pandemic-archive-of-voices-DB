7c2e735192d2166aec122f98c91c055df944debd
mneu windows <alberto.harres@gmail.com>
2021-08-30 19:03:24 +0200

    migrate database to mongodb

---
+++ server/migrate_to_mongodb.js
@@ -0,0 +1,55 @@
+const { ConnectContactLens } = require("aws-sdk");
+const mongoose = require ("mongoose")
+require('dotenv').config()
+const fs = require('fs')
+
+var db_json = fs.readFileSync('public/db/data.json')
+var db_data = JSON.parse(db_json)
+
+const mongoUri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOSTNAME}/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+
+const Audio = mongoose.model('Audio', mongoose.Schema({
+    id: String,
+    name: String,
+    path: String,
+    text: String,
+    user_id: String,
+    duration: Number,
+    lang: Object
+}));
+
+const User = mongoose.model('User', mongoose.Schema({
+    name: String,
+    id: String
+}));
+  
+mongoose.connect(mongoUri, { useNewUrlParser: true }, function (err, res) {    
+    if (err) {
+        console.error(err)
+        throw err
+    }
+    db_data.audios.map((audio) => {
+        return new Audio({
+            id: audio.id + "",
+            name: audio.from,
+            path: audio.file.replace("db", "https://archive-of-voices.s3.eu-central-1.amazonaws.com"),
+            text: audio.text,
+            user_id: audio.from_id + "",
+            duration: audio.duration_seconds,
+            lang: audio.lang
+        }).save(function (err) {
+            if (err) return console.error(err);
+            console.log(`saved to ${audio.id} database!`,)
+        });
+    })
+
+    db_data.speakers.map((user) => {
+        return new User({
+            name: user.speaker,
+            id: user.id + ""
+        }).save(function (err) {
+            if (err) return console.error(err);
+            console.log(`saved to ${user.id} database!`,)
+        });
+    })
+})
