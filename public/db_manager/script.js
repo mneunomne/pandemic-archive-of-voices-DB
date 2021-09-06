6ad0bd284ca194b7bde042ab1cc7598af9eff047
mneu windows <alberto.harres@gmail.com>
2021-09-06 18:35:09 +0200

    db_manager

--- public/db_manager/script.js
+++ public/db_manager/script.js
@@ -13,7 +13,7 @@ const getDatabaseData = () => {
 
 const renderTable = () => {
   $("#table_body").empty()
-  var table_body = json_data.audios.map((audio, index) => {
+  var table_body = json_data.audios.reverse().map((audio, index) => {
     let action_button = audio.disabled === true ? "Enable" : "Disable"; 
     let status = audio.disabled === true ? "table-danger" : "table-success";
     let btn = audio.disabled === true ? "btn-primary" : "btn-warning"
@@ -21,13 +21,14 @@ const renderTable = () => {
       <tr class="${status}">
         <th scope="row">${index}</th>
         <td>
-          <audio controls>
+          <audio preload="none" controls>
             <source src="${audio.path}" type="audio/wav">
           </audio>
         </td>
         <td>${audio.id}</td>
         <td>${audio.user_id}</td>
-        <td>${audio.text}</td>
+        <td><input data-text-id="${audio.id}" class="form-control form-control-sm" type="text" value="${audio.text}" readonly></input></td>
+        <td><button data-id="${audio.id}" type="button" class="btn btn-primary edit">Edit</button></td>
         <td>${audio.duration}</td>
         <td>${audio.lang.code}</td>
         <td><button data-id="${audio.id}" type="button" class="btn ${btn} ${action_button.toLowerCase()}">${action_button}</button></td>
@@ -59,7 +60,28 @@ const addActionEvents = () => {
     let audio_data = json_data.audios.find(a => a.id === id)
     audio_data.deleted = true
     updateAudio(audio_data)
-})
+  })
+
+  $("button.edit").click(function () {
+    let id = $(this).data('id') + ""
+    let input = $(`[data-text-id=${id}]`)
+    let audio_data = json_data.audios.find(a => a.id === id)
+    if ($(this).hasClass("btn-success")) {
+      input.attr("readonly", true)
+      $(this).text("Edit")
+      $(this).removeClass("btn-success")
+      let text = input.val()
+      audio_data.text = text
+      updateAudio(audio_data)
+    } else {
+      input.attr("readonly", false)
+      $(this).text("Save")
+      $(this).addClass("btn-success")
+    }
+    // let audio_data = json_data.audios.find(a => a.id === id)
+    // audio_data.deleted = true;
+    // updateAudio(audio_data)
+  })
 }
 
 const onDataLoaded = () => {
@@ -68,6 +90,7 @@ const onDataLoaded = () => {
 }
 
 const updateAudio = (audio_data) => {
+  modal()
   $.ajax({
     type: "PUT",
     url: `${server_url}/api/audio`,
@@ -80,14 +103,19 @@ const updateAudio = (audio_data) => {
 
 const onPutSuccess = (res) => {
   console.log("Sucess!", res)
+  $('.modal').modal('hide');
   getDatabaseData()
-  
   // renderTable();
   // addActionEvents();
 }
 
 const onPutError = (res) => {
   console.error("Error!", res)
+  $('.modal').modal('hide');
+}
+
+function modal(){
+  $('.modal').modal('show');
 }
 
 getDatabaseData();
